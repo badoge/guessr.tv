47e50fd474dda31f8c9b1e5683a15014bd559df2
badoge <18620902+badoge@users.noreply.github.com>
2023-10-30 16:50:03 +0400

    donk bingo test

---
+++ js/bingo.js
@@ -0,0 +1,14 @@
+const elements = {
+  cells: document.querySelectorAll(".bingo-cell"),
+  toastContainer: document.getElementById("toastContainer"),
+  twitchEmbed: document.getElementById("twitchEmbed"),
+  nextStream: document.getElementById("nextStream"),
+};
+
+window.onload = async function () {
+  for (let index = 0; index < elements.cells.length; index++) {
+    elements.cells[index].onclick = (event) => {
+      event.target.classList.toggle("filled");
+    };
+  }
+}; //onload
